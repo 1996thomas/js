15b5fa8266fe050521328228f03af8f89697c9ba
Thomas <thomas.kanthack@gmail.com>
2023-03-03 09:37:26 +0100

    second_commit

--- script_arn.js
+++ script_arn.js
@@ -1,38 +1,56 @@
-
-function dna(arn){
-  let protein = ""
-  let proteinArray = []
-  for (let i = 0; i < arn.length; i+=3) {
-    codons = arn.slice(i,i+3)
-      if(codons = 'UCU' || 'UCC' || 'UCA' || 'UCG' || 'AGU' || 'AGC'){
-        protein = "Sérine"
-        proteinArray.push(protein)
-      }
-      if(codons = "CCU" || "CCC" || "CCA" || "CCG"){
-        protein = "Proline";
-        proteinArray.push(protein);
-      }
-      if(codons = "UUA" || "UUG"){
-        protein = "Leucine";
-        proteinArray.push(protein);
-      }
-      if(codons = "UUU" || "UUG"){
-        protein = "Phénylalanine";
-        proteinArray.push(protein);
-      }
-      if(codons = "CGU" || "CGC" || "CGA" || "CGG" || "AGA" || "AGG"){
-        protein = "Arginine";
-        proteinArray.push(protein);
-      }
-      if(codons = "UAU" || "UAC"){
-        protein = "Tyrosine";
-        proteinArray.push(protein);
-      }
-
-
+function dna(arn) {
+  let protein = "";
+  let proteinArray = [];
+  for (let i = 0; i < arn.length; i += 3) {
+    codons = arn.slice(i, i + 3);
+    if (
+      codons == "UCU" ||
+      codons == "UCC" ||
+      codons == "UCA" ||
+      codons == "UCG" ||
+      codons == "AGU" ||
+      codons == "AGC"
+    ) {
+      protein = "Sérine";
+      proteinArray.push(protein);
+    } else if (
+      codons == "CCU" ||
+      codons == "CCC" ||
+      codons == "CCA" ||
+      codons == "CCG"
+    ) {
+      protein = "Proline";
+      proteinArray.push(protein);
+    } else if (codons == "UUA" || codons == "UUG") {
+      protein = "Leucine";
+      proteinArray.push(protein);
+    } else if (codons == "UUU" || codons == "UUC") {
+      protein = "Phénylalanine";
+      proteinArray.push(protein);
+    } else if (
+      codons == "CGU" ||
+      codons == "CGC" ||
+      codons == "CGA" ||
+      codons == "CGG" ||
+      codons == "AGA" ||
+      codons == "AGG"
+    ) {
+      protein = "Arginine";
+      proteinArray.push(protein);
+    } else if (codons == "UAU" || codons == "UAC") {
+      protein = "Tyrosine";
+      proteinArray.push(protein);
     }
   }
+  console.log(proteinArray);
+}
 
-  // dna("CCUCGCCGGUACUUCUCG")
-  // console.log(dna("CCGUCGUUGCGCUACAGC"))
-  // console.log(dna("CCUCGCCGGUACUUCUCG")) 
+dna("CCGUCGUUGCGCUACAGC");
+dna("CCUCGCCGGUACUUCUCG");
+
+CCU 
+CGC 
+CGG 
+UAC 
+UUC 
+UCG
